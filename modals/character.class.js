f103ac11d87da08bea4efd94983e291ce349b3ef
Vladislav Kraemer <v21kraemer@gmail.com>
2025-06-21 23:16:06 +0200

    add constructor to classes

--- modals/character.class.js
+++ modals/character.class.js
@@ -1,5 +1,8 @@
 class Character extends MovableObject {
 
+    constructor() {
+        super().loadImage("img/2_character_pepe/1_p1_stand.pngimg/2_character_pepe/2_walk/W-21.png");
+    }
 
     jump() {
         console.log("Jumping");
--- modals/movable-object.class.js
+++ modals/movable-object.class.js
@@ -3,11 +3,16 @@ class MovableObject {
   y = 400;
   img;
 
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
   moveRight() {
     console.log("Moving right");
-    }
-    
-    moveLeft() {
+  }
+
+  moveLeft() {
     console.log("Moving left");
-    }
+  }
 }
