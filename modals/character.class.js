494d37ec38bb98e4f54b7c67d3b9996827aebdf2
Vladislav Kraemer <v21kraemer@gmail.com>
2025-06-27 23:55:16 +0200

    fixed character.class

--- modals/character.class.js
+++ modals/character.class.js
@@ -4,7 +4,5 @@ class Character extends MovableObject {
         super().loadImage("img/2_character_pepe/1_p1_stand.pngimg/2_character_pepe/2_walk/W-21.png");
     }
 
-    jump() {
-        console.log("Jumping");
-    }
+
 }
