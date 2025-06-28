e7754da2556a331c3677e175566dd353962231da
Vladislav Kraemer <v21kraemer@gmail.com>
2025-06-28 22:47:21 +0200

    add new code in gamee.js

--- js/game.js
+++ js/game.js
@@ -1,9 +1,9 @@
 let canvas;
-let ctx;
+let world;
 
 function init() {
   canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
+  world = new World(canvas);
 
   console.log("My character is, ", world.character);
 }
