45a26ba616c7eb99652238041a5ad0f2603ef855
Vladislav Kraemer <v21kraemer@gmail.com>
2025-07-12 23:33:59 +0200

    add new code to world.class.js

--- modals/world.class.js
+++ modals/world.class.js
@@ -12,7 +12,5 @@ class World {
         this.draw();
     }
 
-    draw() {
-       this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height); 
-    }
+
 }
